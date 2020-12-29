8a8273fc0c812999fd61f88022a588da09104676
Gaargi Haval <gaargi.haval@gmail.com>
2020-12-28 21:33:36 -0800

    Hello there

---
+++ Second/Second/SecondApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondApp.swift
+//  Second
+//
+//  Created by user925815 on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct SecondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
